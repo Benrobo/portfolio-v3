4a9a42026b6e4b9d7f2824ea3ff833787eb25d2a
Benaiah <65370456+Benrobo@users.noreply.github.com>
2022-11-15 06:26:27 -0800

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -84,7 +84,7 @@ export default function Header({ children }) {
                         <div className={`relative top-[50px] flex align-start items-start justify-start w-full`}>
                             <div data-aos="zoom-in-left" className={`w-[50%] mr-[20px] flex flex-row items-center justify-start`}>
                                 <h1 className={` text-[35px] pt-[10px] pr-[10px] pb-0 pl-0 `}>
-                                    {new Date().getFullYear() - parseInt(usersInfo.tech_year)}
+                                    {(new Date().getFullYear() - parseInt(usersInfo.tech_year))+1}
                                 </h1>
                                 <span className={` w-[50px] text-white-300 text-[10px] `}>
                                     Years of Experience
